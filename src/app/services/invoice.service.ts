bfaf4530afc101c8306af35b8a8f96fda8fdf8d5
松本一将 <kazumasa.matsumoto62@gmail.com>
2020-06-17 17:52:31 +0900

    わかば祭り6/18

--- src/app/services/invoice.service.ts
+++ src/app/services/invoice.service.ts
@@ -15,16 +15,16 @@ export class InvoiceService {
       priceImagePath: 'assets/img/priceImage/yenImage.png',
       coinImagePath: 'assets/img/priceImage/coin.png'
     },
-    {
-      title: 'ビックゼリー',
-      url: 'big-zeri',
-      icon: 'warning',
-      imgPath: 'assets/img/qrCode/big-zeri.png',
-      itemImagePath: 'assets/img/itemImage/bigZeriImage.png',
-      xym: '20 円',
-      priceImagePath: 'assets/img/priceImage/yenImage.png',
-      coinImagePath: 'assets/img/priceImage/coin2.png'
-    },
+    // {
+    //   title: 'ビックゼリー',
+    //   url: 'big-zeri',
+    //   icon: 'warning',
+    //   imgPath: 'assets/img/qrCode/big-zeri.png',
+    //   itemImagePath: 'assets/img/itemImage/bigZeriImage.png',
+    //   xym: '20 円',
+    //   priceImagePath: 'assets/img/priceImage/yenImage.png',
+    //   coinImagePath: 'assets/img/priceImage/coin2.png'
+    // },
     {
       title: 'チロルチョコ',
       url: 'chiroru-choko',
@@ -35,55 +35,55 @@ export class InvoiceService {
       priceImagePath: 'assets/img/priceImage/yenImage.png',
       coinImagePath: 'assets/img/priceImage/coin2.png'
     },
-    {
-      title: 'カードくじ',
-      url: 'card',
-      icon: 'alert-circle',
-      imgPath: 'assets/img/qrCode/card.png',
-      itemImagePath: 'assets/img/itemImage/cardImage.png',
-      xym: '20 円',
-      priceImagePath: 'assets/img/priceImage/yenImage.png',
-      coinImagePath: 'assets/img/priceImage/coin2.png'
-    },
-    {
-      title: 'ガム',
-      url: 'gum',
-      icon: 'alert-circle',
-      imgPath: 'assets/img/qrCode/gum.png',
-      itemImagePath: 'assets/img/itemImage/gumImage.png',
-      xym: '10 円',
-      priceImagePath: 'assets/img/priceImage/yenImage.png',
-      coinImagePath: 'assets/img/priceImage/coin.png'
-    },
-    {
-      title: 'うまい棒',
-      url: 'umaibou',
-      icon: 'alert-circle',
-      imgPath: 'assets/img/qrCode/umaiBou.png',
-      itemImagePath: 'assets/img/itemImage/umaiBouImage.png',
-      xym: '10 円',
-      priceImagePath: 'assets/img/priceImage/yenImage.png',
-      coinImagePath: 'assets/img/priceImage/coin.png'
-    },
-    {
-      title: '5円チョコ',
-      url: '5yen',
-      icon: 'alert-circle',
-      imgPath: 'assets/img/qrCode/goYen.png',
-      itemImagePath: 'assets/img/itemImage/goYenImage.png',
-      xym: '5 円',
-      priceImagePath: 'assets/img/priceImage/yenImage.png',
-      coinImagePath: 'assets/img/priceImage/goYenCoin.png'
-    },
+    // {
+    //   title: 'カードくじ',
+    //   url: 'card',
+    //   icon: 'alert-circle',
+    //   imgPath: 'assets/img/qrCode/card.png',
+    //   itemImagePath: 'assets/img/itemImage/cardImage.png',
+    //   xym: '20 円',
+    //   priceImagePath: 'assets/img/priceImage/yenImage.png',
+    //   coinImagePath: 'assets/img/priceImage/coin2.png'
+    // },
+    // {
+    //   title: 'ガム',
+    //   url: 'gum',
+    //   icon: 'alert-circle',
+    //   imgPath: 'assets/img/qrCode/gum.png',
+    //   itemImagePath: 'assets/img/itemImage/gumImage.png',
+    //   xym: '10 円',
+    //   priceImagePath: 'assets/img/priceImage/yenImage.png',
+    //   coinImagePath: 'assets/img/priceImage/coin.png'
+    // },
+    // {
+    //   title: 'うまい棒',
+    //   url: 'umaibou',
+    //   icon: 'alert-circle',
+    //   imgPath: 'assets/img/qrCode/umaiBou.png',
+    //   itemImagePath: 'assets/img/itemImage/umaiBouImage.png',
+    //   xym: '10 円',
+    //   priceImagePath: 'assets/img/priceImage/yenImage.png',
+    //   coinImagePath: 'assets/img/priceImage/coin.png'
+    // },
+    // {
+    //   title: '5円チョコ',
+    //   url: '5yen',
+    //   icon: 'alert-circle',
+    //   imgPath: 'assets/img/qrCode/goYen.png',
+    //   itemImagePath: 'assets/img/itemImage/goYenImage.png',
+    //   xym: '5 円',
+    //   priceImagePath: 'assets/img/priceImage/yenImage.png',
+    //   coinImagePath: 'assets/img/priceImage/goYenCoin.png'
+    // },
     {
       title: 'わかばくじ',
       url: 'wakaba',
       icon: 'alert-circle',
       imgPath: 'assets/img/qrCode/wakaba.png',
       itemImagePath: 'assets/img/itemImage/wakabaImage.png',
-      xym: '10 円',
+      xym: '20 円',
       priceImagePath: 'assets/img/priceImage/yenImage.png',
-      coinImagePath: 'assets/img/priceImage/coin.png'
+      coinImagePath: 'assets/img/priceImage/coin2.png'
     },
     {
       title: 'レアカラーおりがみ',
@@ -95,16 +95,16 @@ export class InvoiceService {
       priceImagePath: 'assets/img/priceImage/yenImage.png',
       coinImagePath: 'assets/img/priceImage/coin.png'
     },
-    {
-      title: 'スーパーボールすくい',
-      url: 'superBall',
-      icon: 'alert-circle',
-      imgPath: 'assets/img/qrCode/superBall.png',
-      itemImagePath: 'assets/img/itemImage/superBallImage.png',
-      xym: '20 円',
-      priceImagePath: 'assets/img/priceImage/yenImage.png',
-      coinImagePath: 'assets/img/priceImage/coin2.png'
-    },
+    // {
+    //   title: 'スーパーボールすくい',
+    //   url: 'superBall',
+    //   icon: 'alert-circle',
+    //   imgPath: 'assets/img/qrCode/superBall.png',
+    //   itemImagePath: 'assets/img/itemImage/superBallImage.png',
+    //   xym: '20 円',
+    //   priceImagePath: 'assets/img/priceImage/yenImage.png',
+    //   coinImagePath: 'assets/img/priceImage/coin2.png'
+    // },
     {
       title: 'こんにゃくぜりー',
       url: 'connyaku',
@@ -115,36 +115,36 @@ export class InvoiceService {
       priceImagePath: 'assets/img/priceImage/yenImage.png',
       coinImagePath: 'assets/img/priceImage/coin2.png'
     },
-    {
-      title: 'お食事券',
-      url: 'oshokuji',
-      icon: 'alert-circle',
-      imgPath: 'assets/img/qrCode/oshokuji.png',
-      itemImagePath: 'assets/img/itemImage/oshokujiImage.png',
-      xym: '50 円',
-      priceImagePath: 'assets/img/priceImage/yenImage.png',
-      coinImagePath: 'assets/img/priceImage/50yen.png'
-    },
-    {
-      title: 'かたぬき',
-      url: 'katanuki',
-      icon: 'alert-circle',
-      imgPath: 'assets/img/qrCode/katanuki.png',
-      itemImagePath: 'assets/img/itemImage/katanukiImage.png',
-      xym: '20 円',
-      priceImagePath: 'assets/img/priceImage/yenImage.png',
-      coinImagePath: 'assets/img/priceImage/coin2.png'
-    },
-    {
-      title: '射的',
-      url: 'shateki',
-      icon: 'alert-circle',
-      imgPath: 'assets/img/qrCode/shateki.png',
-      itemImagePath: 'assets/img/itemImage/shatekiImage.png',
-      xym: '20 円',
-      priceImagePath: 'assets/img/priceImage/yenImage.png',
-      coinImagePath: 'assets/img/priceImage/coin2.png'
-    },
+    // {
+    //   title: 'お食事券',
+    //   url: 'oshokuji',
+    //   icon: 'alert-circle',
+    //   imgPath: 'assets/img/qrCode/oshokuji.png',
+    //   itemImagePath: 'assets/img/itemImage/oshokujiImage.png',
+    //   xym: '50 円',
+    //   priceImagePath: 'assets/img/priceImage/yenImage.png',
+    //   coinImagePath: 'assets/img/priceImage/50yen.png'
+    // },
+    // {
+    //   title: 'かたぬき',
+    //   url: 'katanuki',
+    //   icon: 'alert-circle',
+    //   imgPath: 'assets/img/qrCode/katanuki.png',
+    //   itemImagePath: 'assets/img/itemImage/katanukiImage.png',
+    //   xym: '20 円',
+    //   priceImagePath: 'assets/img/priceImage/yenImage.png',
+    //   coinImagePath: 'assets/img/priceImage/coin2.png'
+    // },
+    // {
+    //   title: '射的',
+    //   url: 'shateki',
+    //   icon: 'alert-circle',
+    //   imgPath: 'assets/img/qrCode/shateki.png',
+    //   itemImagePath: 'assets/img/itemImage/shatekiImage.png',
+    //   xym: '20 円',
+    //   priceImagePath: 'assets/img/priceImage/yenImage.png',
+    //   coinImagePath: 'assets/img/priceImage/coin2.png'
+    // },
   ];
 
   constructor() { }
